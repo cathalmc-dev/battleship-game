918ed68b6f1f5de4b74a4f3d938c23efd477b70f
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-03 18:43:06 +0000

    Add function to set the sizing factor on the grids

--- run.py
+++ run.py
@@ -1,3 +1,23 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+from random import randint
+
+
+size = None
+cpu_board = []
+player_board = []
+boards = cpu_board + player_board
+
+def set_grid_size():
+    global size
+    size = input('Set grid row and column size: ')
+
+
+def main():
+    """
+    Run all program functions
+    """
+    set_grid_size()
+    print(size)
+
+
+print("Welcome to Battleships!")
+main()
