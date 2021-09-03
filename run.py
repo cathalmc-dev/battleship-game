a60407c7554072537707b9fdee5e596f025299a4
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-03 19:34:58 +0000

    Add coordinates to grids

--- run.py
+++ run.py
@@ -1,6 +1,8 @@
 from random import randint
 
 size = None
+LETTERS = [" ", "A", "B", "C", "D", "E", "F", "G", "H", "I",
+"J", "K", "L", "M", "N", "O"]
 cpu_board = []
 player_board = []
 boards = cpu_board + player_board
@@ -25,8 +27,9 @@ def set_grid_size():
 
 
 def initialize_board(board):
+    board.append(LETTERS[:size + 1])
     for i in range(size):
-        board.append(["0"] * size)
+        board.append([f"{i + 1}"] + ["0"] * size)
 
 
 def print_board(board):
