232b894f87f3d5139383379a4d684cc06a2b41c9
cathalmc-dev <cathalmc.dev@gmail.com>
2021-09-03 18:57:17 +0000

    Add error handling to grid size selection function.

--- run.py
+++ run.py
@@ -1,14 +1,27 @@
 from random import randint
 
-
 size = None
 cpu_board = []
 player_board = []
 boards = cpu_board + player_board
 
 def set_grid_size():
+    print("Please set the grid size using 1 integer.")
+    print("The number of rows and columns will be equal.")
+    message = "Please enter an integer between 5 and 15: "
     global size
-    size = input('Set grid row and column size: ')
+    valid = False
+    while not valid:
+        size = input(message)
+        try:
+            size = int(size)
+        except ValueError:
+            message = "Please enter integer values only: "
+        else:
+            valid = 4 < size < 16
+            if not valid:
+                message = "Enter an integer between 5 and 15: "
+    print(f"You have entered {size}.")
 
 
 def main():
